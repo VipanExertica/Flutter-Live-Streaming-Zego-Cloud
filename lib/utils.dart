f254d691227f784a14ce6d80033450ae2e04071f
Vipan Kumar <vipankumar7607@gmail.com>
2023-07-10 00:22:43 +0530

    New Flutter App

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,7 @@
 
 class Utils {
 
-  static int appId =  12121; // enter your id
-  static String appSignin = "eneter your id" ;
+  static int appId =  2099878866; // enter your id
+  static String appSignin = "3116f68eb90d9e2caacc6ba95ca5d938cb9699ced53841bda716107bef636c83";
 
 }
